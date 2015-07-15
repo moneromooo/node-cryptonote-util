4b8381646a9c3f890171468c2d2fac6b2966c332
moneromooo <moneromoo@nowhere.nowhere.nowhere>
2015-07-15 19:56:41 +0100

    crypnonight-lite support

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -11,3 +11,6 @@
 
 #define CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW            60
 #define CRYPTONOTE_DISPLAY_DECIMAL_POINT                12
+
+// TODO: correct height
+#define LIGHT_HASH_START_HEIGHT                         999
